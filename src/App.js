f4f669e41b4da715c3709fd70d68a6341ee57583
jeff <jlam3950@gmail.com>
2023-02-16 10:08:58 -0500

    setting up components. router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import About from './components/About';
+import Contact from './components/Contact';
+import Main from './components/Main';
+import Navbar from './components/Navbar';
+import Search from './components/Search';
+import Footer from './components/Footer';
+import Login from './components/Login';
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Main/>
+      {/* <Routes>
+        <Route path = '/' element = {<Main />}></Route>
+      </Routes> */}
     </div>
   );
 }
