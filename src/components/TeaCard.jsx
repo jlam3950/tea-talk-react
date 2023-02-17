af3f6b291c2418981ffac194b9ef30598d17c9c8
Brian Russell <49926177+Mr-Russell@users.noreply.github.com>
2023-02-16 19:55:26 -0500

    Added TeaCard and TeaForm components. Also changed title in HTML.

---
+++ src/components/TeaCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TeaCard = () => {
+  return (
+    <div>Tea Card</div>
+  )
+}
+
+export default TeaCard
---
+++ src/components/TeaForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TeaForm = () => {
+  return (
+    <div>Tea Form</div>
+  )
+}
+
+export default TeaForm
