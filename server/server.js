46b6d904a7522f8ad0230ac7df14f08b5a07d5b6
Brian Russell <49926177+Mr-Russell@users.noreply.github.com>
2023-02-20 11:38:44 -0500

    Added basic routing structure.

--- server/server.js
+++ server/server.js
@@ -18,6 +18,12 @@ mongoose.connect(
     }
 )
 
+const userRouter = require("./routes/userRouter.js");
+app.use("/users", userRouter);
+
+const teaRouter = require("./routes/teaRouter.js");
+app.use("/teas", teaRouter);
+
 app.listen(port, () => {
     console.log(`app is running on port ${port}`);
 })
