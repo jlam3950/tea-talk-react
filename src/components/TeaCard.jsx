a3ac7169ca7ab220fb4fea8c9fd1fe95530c1d2e
jeff <jlam3950@gmail.com>
2023-02-20 16:04:57 -0500

    added props to teaCard. working on save feature

--- src/components/TeaCard.jsx
+++ src/components/TeaCard.jsx
@@ -1,32 +1,34 @@
 import React from "react";
 import { FaPlus } from 'react-icons/fa';
 
-const TeaCard = () => {
+const TeaCard = (props) => {
+
   return (
     <div className="teaCard d-flex border my-3 py-4 mx-2 px-4">
       <div className="col-4 d-flex justify-content-center align-items-center">
-        <img className = 'teaCardImg' src="/images/doubleSpiceChai.png" alt="" />
+        <img className = 'teaCardImg' src={props.img} alt="" />
       </div>
       <div className="col-6 border">
         <div className="mr-2">
           <div className="border"> 
-            Tea Name 
+            {props.name}
           </div>
           <div className="d-flex">
             <div className="col-6">
-              Brand
+              {props.brand}
             </div>
             <div className="col-6">
-              Type
+              {props.type}
             </div>
           </div>
           <div className="border">
-            Rating
+            {props.rating}
           </div>
         </div>
       </div>
       <div className="teaPlus col-2 border d-flex justify-content-center align-items-center">
-        <FaPlus />
+        <FaPlus onClick = {(e) => console.log(props)}/>
+        {/* need to pass this back up to the parent to store in array */}
       </div>
     </div>
   );
