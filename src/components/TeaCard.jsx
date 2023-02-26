2afb5bc40485df2ba4d3de901d4a37bbd8156e6d
jeff <jlam3950@gmail.com>
2023-02-25 19:02:16 -0500

    updated teaCard UI on main page

--- src/components/TeaCard.jsx
+++ src/components/TeaCard.jsx
@@ -4,18 +4,18 @@ import { FaPlus } from 'react-icons/fa';
 const TeaCard = (props) => {
 
   return (
-    <div className="teaCard border d-flex my-3 py-4 mx-2 px-4">
+    <div className="teaCard d-flex my-2 py-3 mx-2 px-4">
       <div className="col-4 d-flex justify-content-center align-items-center">
         {/* <img className = 'teaCardImg' src={props.img} alt="" /> */}
         {/* will update ^ teas once images are loaded in db */}
         <img className = 'teaCardImg' src='/images/doubleSpiceChai.png' alt="" />
       </div>
-      <div className="col-6 border">
+      <div className="col-6">
         <div className="mr-2">
-          <div className="border"> 
+          <div className="cardTitle my-0 py-0"> 
             {props.name}
           </div>
-          <div className="d-flex">
+          <div className="d-flex cardSubtitleAndType">
             <div className="col-6">
               {props.brand}
             </div>
@@ -23,8 +23,11 @@ const TeaCard = (props) => {
               {props.type}
             </div>
           </div>
-          <div className="border">
+          <div className="">
             {props.rating}
+          </div>
+          <div className="">
+            "Black tea is a kind of tea made from leaves of Camellia sinensis. Often, it is stronger in taste than other varieties of tea, like green tea or oolong."
           </div>
         </div>
       </div>
