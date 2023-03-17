2526edcaaf3d78f038cd95a4cdc535e2be118cff
Brian Russell <49926177+Mr-Russell@users.noreply.github.com>
2023-03-17 12:01:27 -0400

    Added the mongoose populate funtionality so teaLists return all tea info and not just tea IDs.

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -6,12 +6,13 @@ const bcrypt = require("bcryptjs");
 // Get User with given ID
 router.get("/:id", async (req, res) => {
     try {
-        const user = await User.findById(req.params.id, {password: false});
+        const user = await User.findById(req.params.id, {password: false}).populate("teaLists.$*");
         if (user == null) {
             res.status(404).json({ message: `Cannot find User with ID ${req.params.id}` });
         }
         res.status(200).json(user)
     } catch (err) {
+        console.error(err)
         res.status(500).json({ message: err.message })
     };
 
@@ -50,7 +51,7 @@ router.post("/login", async (req, res) => {
         if (user == null) {
             res.status(404).json({ message: `Cannot find User with Username "${req.body.username}"` });
         } else if (user && checkPassword) {
-            const userInfo = await User.findById(user.id, {password: false});
+            const userInfo = await User.findById(user.id, {password: false}).populate("teaLists.$*");
             req.session.user = userInfo;
             res.status(200).json(userInfo)
         } else {
