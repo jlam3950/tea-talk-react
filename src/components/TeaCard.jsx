f9e9673eb28ac1aefaea19f26c9b560284b04644
Brian Russell <49926177+Mr-Russell@users.noreply.github.com>
2023-04-17 17:25:10 -0400

    Updated styling for TeaRating component

--- src/components/TeaCard.jsx
+++ src/components/TeaCard.jsx
@@ -4,11 +4,10 @@ import { FaPlusSquare, FaStar } from 'react-icons/fa';
 import { ListContext } from '../App';
 import { useContext, useState } from 'react';
 import Nav from 'react-bootstrap/Nav';
-import { Rating } from 'react-simple-star-rating';
 import { TeaRating } from "./TeaRating";
 
 const TeaCard = (props) => {
-  const { refreshTeaList, userProfile, setUserProfile, setAlertFlag, setAlertInfo } = useContext(ListContext);
+  const { refreshTeaList, userProfile, setAlertFlag, setAlertInfo } = useContext(ListContext);
   const { tea } = props
   const [modalShow, setModalShow] = useState(false);
   const [selectedtea, setselectedtea] = useState({});
@@ -17,7 +16,7 @@ const TeaCard = (props) => {
     console.log(selectedtea);
     refreshTeaList(userProfile._id)
     setModalShow(true);
-    setselectedtea(props);
+    setselectedtea(tea);
   }
 
   const setAlert = (alert) => {
@@ -31,41 +30,43 @@ const TeaCard = (props) => {
 
   <Nav.Link href="#action2" style ={{color: 'white'}}>My Teas</Nav.Link>
   return (
-    <div className="teaCard d-flex my-3 py-2 px-2">
-      <div className="col-3 d-flex justify-content-center align-items-center">
-        <img className = 'teaCardImg' src={tea.imageURL} alt="" />
-      </div>
-      <div className="col-7">
-        <div className="mr-2">
-          <div className="cardTitle my-0 py-0"> 
-            <Nav.Link href={`/teaPage/${tea._id}}`} style ={{color: 'black'}}>{tea.name}</Nav.Link>
-          </div>
-          <div className="d-flex cardSubtitleAndType">
-            <div className=" cardSubtitle col-4">
-              {tea.brand}
+    <div className="my-3">
+      <div className="teaCard d-flex py-1 px-2">
+        <div className="col-3 d-flex justify-content-center align-items-center">
+          <img className = 'teaCardImg' src={tea.imageURL} alt="" />
+        </div>
+        <div className="col-7">
+          <div className="mr-2">
+            <div className="cardTitle my-0 py-0"> 
+              <Nav.Link href={`/teaPage/${tea._id}}`} style ={{color: 'black'}}>{tea.name}</Nav.Link>
             </div>
-            <div className="cardSubtitle col-2">
-              {tea.type}
+            <div className="d-flex cardSubtitleAndType">
+              <div className=" cardSubtitle col-4">
+                {tea.brand}
+              </div>
+              <div className="cardSubtitle col-2">
+                {tea.type}
+              </div>
             </div>
-          </div>
-          <div className="cardDescription">
-            "Black tea is a kind of tea made from leaves of Camellia sinensis. Often, it is stronger in taste than other varieties of tea, like green tea or oolong."
+            <div className="cardDescription">
+              "Black tea is a kind of tea made from leaves of Camellia sinensis. Often, it is stronger in taste than other varieties of tea, like green tea or oolong."
+            </div>
           </div>
         </div>
-          <TeaRating tea={tea}/>
+        <div className="teaPlus col-2 d-flex justify-content-center align-items-center">
+          { userProfile.length !== 0 ? 
+              <>
+              <FaPlusSquare onClick = {() => openModal(props)}/>
+                  <AddTeaModal
+                  show={modalShow}
+                  onHide={() => setModalShow(false)}
+                  selectedtea = {selectedtea}
+                  /> 
+              </>: 
+          <FaPlusSquare onClick = {() => setAlert('Sign in to save tea...')}/> }
         </div>
-      <div className="teaPlus col-2 d-flex justify-content-center align-items-center">
-        { userProfile.length !== 0 ? 
-             <>
-             <FaPlusSquare onClick = {() => openModal(props)}/>
-                <AddTeaModal
-                show={modalShow}
-                onHide={() => setModalShow(false)}
-                selectedtea = {selectedtea}
-                /> 
-            </>: 
-        <FaPlusSquare onClick = {() => setAlert('Sign in to save tea...')}/> }
       </div>
+      <TeaRating tea={tea}/>
     </div>
   );
 };
